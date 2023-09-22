35560cbe27283d7b241bd9168eadf1f98c3254ba
flefur <f.lefur@djust.io>
2023-09-22 16:13:00 +0200

    Tried to implement dictionnaries

--- src/dtos/dictionnary.ts
+++ src/dtos/dictionnary.ts
@@ -1,7 +1,3 @@
 import Queue from "../utils/queue";
 
-export interface Dictionnary {
-    defaultQueue: Queue;
-    groupe1Queue: Queue;
-    groupe2Queue: Queue;
-}
+export default new Map<string, Queue>;
--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -1,18 +1,28 @@
 import express from 'express';
 import { Request, Response } from 'express';
 import Queue from '../utils/queue';
+import Queues from '../dtos/dictionnary'
 
 const router = express.Router();
 
-router.post('/cancel-book', async (req: Request, res: Response) => {
+router.post('/cancel-book',  async (req: Request, res: Response) => {
 
   console.log('hello')
   try {
     let bookingId = req.body.bookingId;
-    
-    Queue.queue.push(bookingId);
+    if (!req.headers['x-api-key']) {
+      Queue.queue.push(bookingId);
+      res.status(400)
+      res.send({})
+      return
+    }
+
+    if (!Queues.has(req.headers['x-api-key'])) {
+      Queues.set(req.headers['x-api-key'], [])
+    }
     console.log(Queue.queue)
-    res.status(200); //Sending code 202 because the action has been queued
+
+    res.status(202); //Sending code 202 because the action has been queued
     res.send({})
   } catch (e) {
     res.send({
